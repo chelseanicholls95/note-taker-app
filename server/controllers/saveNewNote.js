dba0d36966abf77d51b4b139c7f19cdc4c190ecb
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-05-19 19:17:27 +0100

    added uuid and added id to new saved notes

--- server/controllers/saveNewNote.js
+++ server/controllers/saveNewNote.js
@@ -1,7 +1,10 @@
+const uuid = require("uuid");
+
 const { read, write } = require("../../db/database");
 
 const saveNewNote = (req, res) => {
   const data = JSON.parse(read());
+  req.body.id = uuid.v4();
   data.push(req.body);
   write(JSON.stringify(data));
 };
