d837317c544e2735f07c3da101df7af230c13918
Chelsea Nicholls <chelseanicholls95@live.co.uk>
2021-05-19 19:46:08 +0100

    added cors middleware and added notes with id's

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,9 +8,13 @@ const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 
+// middleware
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({ extended: true }));
 app.use(express.static("public"));
+
+// routes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
